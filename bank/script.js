bd0aedd3ad86e1cac2745f81ef55915f28d29f46
Mohammed AlDini <51696292+moaldini@users.noreply.github.com>
2021-01-19 20:39:26 -0600

    Update script.js

--- bank/script.js
+++ bank/script.js
@@ -1,40 +1,41 @@
-"use strict";
-
-var bankingAccount = function() {
-    var accountInfoList = [];
-    function Account(name, deposit) {
-        this.name = name;
-        this.deposit = deposit;
-    }
-
-    var createAccount = function () {
-        let name = document.getElementById("name");
-        let deposite = document.getElementById("deposit");
-
-        if(name.value.trim() == "") {
-            alert("Please enter value in name!");
-            name.focus();
-            return false;
-        }
-        if(deposite.value.trim() == "") {
-            alert("Please enter value in Deposite!");
-            deposite.focus();
-            return false;
+(function() {
+    "use strict";
+    var bankingAccount = function() {
+        var accountInfoList = [];
+        function Account(name, deposit) {
+            this.name = name;
+            this.deposit = deposit;
         }
 
-        let newAccount = new Account(name.value, deposit.value);
-        accountInfoList.push(newAccount);
-        showAccountInfo(accountInfoList);
+        var createAccount = function () {
+            let name = document.getElementById("name");
+            let deposite = document.getElementById("deposit");
+
+            if(name.value.trim() == "") {
+                alert("Please enter value in name!");
+                name.focus();
+                return false;
+            }
+            if(deposite.value.trim() == "") {
+                alert("Please enter value in Deposite!");
+                deposite.focus();
+                return false;
+            }
+
+            let newAccount = new Account(name.value, deposit.value);
+            accountInfoList.push(newAccount);
+            showAccountInfo(accountInfoList);
+        };
+
+        function showAccountInfo(accountlist) {
+            let result = "";
+            for (var account of accountlist) {
+                result += "Account name: " + account.name + " Balance: " + account.deposit + "\n";
+            }
+            document.getElementById("result").value = result;
+        }
+
+        document.getElementById("btn").onclick = createAccount;
     };
-
-    function showAccountInfo(accountlist) {
-        let result = "";
-        for (var account of accountlist) {
-            result += "Account name: " + account.name + " Balance: " + account.deposit + "\n";
-        }
-        document.getElementById("result").value = result;
-    }
-
-    document.getElementById("btn").onclick = createAccount;
-};
-window.onload = bankingAccount;
+    window.onload = bankingAccount;
+})();
